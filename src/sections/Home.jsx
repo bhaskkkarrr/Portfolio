39eb0b2c8fb1dfe124a85682f9d2bbb45f76ccdf
bhaskkkarrr <bhaskarchauhan748@gmail.com>
2026-03-16 19:40:43 +0530

    Made About Me Section

--- src/sections/Home.jsx
+++ src/sections/Home.jsx
@@ -1,5 +1,5 @@
 import { motion } from "motion/react";
-import profile from "../assets/profileeee.png";
+import profile from "../assets/avator.png";
 import Typewriter from "typewriter-effect";
 import { SiGithub } from "react-icons/si";
 import { FaInstagram } from "react-icons/fa";
@@ -7,38 +7,19 @@ import { FaLinkedinIn } from "react-icons/fa";
 
 const Home = () => {
   return (
-    <section id="home" className="w-full h-screen relative ">
+    <section id="home" className="w-full h-fit md:mt-5 mt-15 relative ">
       <div className="absolute inset-0">
         <div className="absolute top-1 left-1 bg-linear-60 from-amethyst-smoke-500 via-dark-amethyst-400 to-dusty-grape-500 w-[30vw] h-[30vw] md:w-[20vw] md:h-[20vw]  max-w-125 max-h-125 rounded-full opacity-10 animate-pulse blur-[60px] "></div>
         <div className=" absolute -bottom-10 -right-5 bg-linear-60 from-amethyst-smoke-500 via-dark-amethyst-400 to-dusty-grape-500 w-[70vw] h-[70vw] md:w-[20vw] md:h-[20vw]  max-w-125 max-h-125 rounded-full opacity-10 animate-pulse blur-[70px]"></div>
       </div>
 
       <div className="relative h-full w-full md:w-xl lg:w-5xl xl:w-7xl max-w-8xl mx-auto px-4 grid grid-cols-1 lg:grid-cols-2 transition-all duration-300">
-        {/* DP */}
-        <div className="flex xl:hidden mt-15 lg:mt-0 relative justify-center items-center">
-          <motion.div
-            initial={{ opacity: 0 }}
-            animate={{
-              opacity: 1,
-              transition: { delay: 0.2, duration: 0.2, ease: "easeIn" },
-              filter: "drop-shadow(0 0 20px #fff)",
-            }}
-            className="h-60 w-60 md:h-80 md:w-80 lg:h-100 lg:w-100 transition-all duration-300"
-          >
-            <img
-              src={profile}
-              alt="profile"
-              quality={100}
-              className="object-contain rounded-full"
-            />
-          </motion.div>
-        </div>
         {/* Name */}
         <div className="flex flex-col h-full justify-center text-center lg:text-left relative">
           <div className="w-full mx-auto max-w-4xl">
             <motion.div className="flex flex-col  lg:items-start space-y-6 xl:space-y-8 ">
               {/* TypeWriter Effect */}
-              <div className="name text-xl lg:text-4xl">
+              <div className="transition-all duration-300 name text-xl md:text-4xl lg:text-4xl">
                 <Typewriter
                   options={{
                     strings: ["Web Developer", "Software Developer"],
@@ -49,15 +30,15 @@ const Home = () => {
               </div>
               {/* Name */}
               <div className="">
-                <div className="text-dark-amethyst-200 text-4xl lg:font-medium lg:text-5xl">
+                <div className="text-dark-amethyst-200 text-4xl transition-all duration-300 font-medium lg:text-5xl">
                   Hello, I'm
                 </div>
-                <div className="text-4xl lg:text-5xl xl:text-6xl lg:font-bold">
+                <div className="text-5xl transition-all duration-300 md:text-5xl xl:text-6xl font-bold">
                   Bhaskar Chauhan
                 </div>
               </div>
               {/* Caption */}
-              <div className="text-base lg:text-2xl">
+              <div className="text-base md:text-xl lg:text-2xl">
                 I build full-stack web applications using the MERN stack,
                 focusing on clean design, performance, and real-world usability.
               </div>
@@ -120,23 +101,21 @@ const Home = () => {
           </div>
         </div>
 
-        <div className="xl:flex hidden mt-15 lg:mt-0 relative justify-center items-center">
+        <div className="lg:flex hidden mt-15 lg:mt-0 relative justify-center items-center">
           <motion.div
             initial={{ opacity: 0 }}
             animate={{
               opacity: 1,
               transition: { delay: 0.2, duration: 0.2, ease: "easeIn" },
-              filter: "drop-shadow(0 0 20px #fff)",
+              filter: "drop-shadow(0 0 20px #826bc7)",
             }}
-            transition={{ type: "spring", stiffness: 300 }}
-            whileHover={{ scale: 1.1 }}
-            className="h-60 w-60 md:h-80 md:w-80 lg:h-100 lg:w-100 "
+            className=""
           >
             <img
               src={profile}
               alt="profile"
               quality={100}
-              className="object-contain rounded-full"
+              className="h-[50vw]"
             />
           </motion.div>
         </div>
